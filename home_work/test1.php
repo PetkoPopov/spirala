1f0482f1bd4c95a5083f4de96f753f8b2b21115d
PetkoPopov <poetpoet2014@mail.ru>
2020-10-13 21:54:58 +0300

    backup copy

---
+++ home_work/test1.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Test;
+class Test1{
+    public function __construct() {
+        echo "TEST 1";;
+    }
+}
+$foo=new Test2(); 
---
+++ home_work/test2.php
@@ -0,0 +1,8 @@
+
+
+
+
+<script type="text/javascript">
+document.while ("xcutter"); 
+        
+</script>
